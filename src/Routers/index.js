d095443b0f70f6b7b97ebbba99181d477a146df5
Ngocminhhuynhle123 <huynhleminhngocafc@gmail.com>
2023-04-11 10:05:16 +0700

    ngoc

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,8 +1,9 @@
 import { lazy } from "react";
 import { Route } from "react-router-dom";
-const routers = [{
+const routers = [
+  {
     path: "/",
-    element: lazy(() => import('./../pages/Jira-Admin/')),
+    element: lazy(() => import("./../pages/Jira-Admin/")),
     nested: [
         {
             path: "/",
@@ -29,20 +30,37 @@ const routers = [{
 },
 {
     path: "/logInjiraa",
-    element: lazy(() => import('./../pages/Jira-Admin/authPages')),
-},
-]
+    element: lazy(() => import("./../pages/Jira-Admin/authPages")),
+  },
+];
 export const renderRouter = () => {
-    return routers.map((router) => {
-        if (router.nested) {
-            return <Route key={router.path} path={router.path} element={<router.element />} >
-                {router.nested.map((item) => {
-                    return <Route key={item.path} path={item.path} element={<item.element />} />
-                })}
-            </Route>
-        } else {
-            return <Route key={router.path} path={router.path} element={<router.element />} />
-
-        }
-    })
-}
+  return routers.map((router) => {
+    if (router.nested) {
+      return (
+        <Route
+          key={router.path}
+          path={router.path}
+          element={<router.element />}
+        >
+          {router.nested.map((item) => {
+            return (
+              <Route
+                key={item.path}
+                path={item.path}
+                element={<item.element />}
+              />
+            );
+          })}
+        </Route>
+      );
+    } else {
+      return (
+        <Route
+          key={router.path}
+          path={router.path}
+          element={<router.element />}
+        />
+      );
+    }
+  });
+};
